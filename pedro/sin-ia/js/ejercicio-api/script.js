95a8bffd629a505a69988f9baf29fecb6a17c518
Stereohead1 <pedrolopezmanera@gmail.com>
2026-03-09 17:45:20 +0100

    Actualizo el CSS de mi Api

--- pedro/sin-ia/js/ejercicio-api/script.js
+++ pedro/sin-ia/js/ejercicio-api/script.js
@@ -16,12 +16,12 @@ async function loadData() {
     for (let i = 0; i < droids.length; i++) {
       const droid = droids[i];
       cardsEl.innerHTML += `
-        <div style="border:1px solid #ccc; padding:10px; margin:5px;">
-          <h3>${droid.name}</h3>
-          <img src="${droid.image}" alt="${droid.name}" style="width:150px;">
-          <p>${droid.description}</p>
-        </div>
-      `;
+  <div class="card">
+    <h3>${droid.name}</h3>
+    <img src="${droid.image}" alt="${droid.name}">
+    <p>${droid.description}</p>
+  </div>
+  `;
     }
 
     statusEl.textContent = `Total de droides: ${droids.length}`;
