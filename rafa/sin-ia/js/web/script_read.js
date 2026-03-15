5d04836af8fb04869273544c95016e22b79429e4
Rafagar123 <rafagarrido17@gmail.com>
2026-03-15 17:21:13 +0100

    Otro commit monstruoso

--- rafa/sin-ia/js/web/script_read.js
+++ rafa/sin-ia/js/web/script_read.js
@@ -2,8 +2,8 @@ const API_URL = 'http://localhost:3000/recipes';
 
 // READ ALL
 
-function getReadCard(nombre, ingredientes, id){
-  return  '<div class = readCard> <a href="read1.html?id='+ id +'"> <h3>' + nombre + '</h3> <p>' + ingredientes + '</p></a> </div> '
+function getReadCard(nombre, ingredientes, id) {
+  return '<div class = readCard> <a href="read1.html?id=' + id + '"> <h3>' + nombre + '</h3> <p>' + ingredientes + '</p></a> </div> '
 }
 
 async function readAll() {
@@ -13,15 +13,15 @@ async function readAll() {
     console.log("data: ", data);
     let info = data;
     let fLen = data.length;
-    for(let i= 0; i<fLen; i++){
-        let myDiv = document.createElement("div");
-        myDiv.setAttribute("id", "div"+info[i].id);
-        document.getElementById("read").appendChild(myDiv);
-        let content = "";
-        content += getReadCard(info[i].nombre, info[i].ingredientes, info[i].id);
-        document.getElementById("div"+info[i].id).innerHTML = content;
+    for (let i = 0; i < fLen; i++) {
+      let myDiv = document.createElement("div");
+      myDiv.setAttribute("id", "div" + info[i].id);
+      document.getElementById("read").appendChild(myDiv);
+      let content = "";
+      content += getReadCard(info[i].nombre, info[i].ingredientes, info[i].id);
+      document.getElementById("div" + info[i].id).innerHTML = content;
     }
-    
+
   } catch (error) {
     console.log("Network error");
   }
--- rafa/sin-ia/js/web/script_update.js
+++ rafa/sin-ia/js/web/script_update.js
@@ -2,7 +2,7 @@ const API_URL = 'http://localhost:3000/recipes';
 
 // READ ALL
 
-function getReadCard(nombre, ingredientes, id) {
+function getReadCard(nombre, ingredientes) {
   return '<div class = readCard><h3>' + nombre + '</h3> <p>' + ingredientes + '</p></div> '
 }
 
@@ -22,10 +22,12 @@ async function readAll() {
       content += getReadCard(info[i].nombre, info[i].ingredientes, info[i].id);
       document.getElementById(info[i].id).innerHTML = content;
       let myButton = document.createElement("button");
-      myButton.setAttribute("id", "btn" + info[i].id);
+      myButton.setAttribute("id", info[i].id);
       myButton.setAttribute("class", "btn");
       myButton.innerHTML = "Edit";
       document.getElementById(info[i].id).appendChild(myButton);
+
+      myButton.addEventListener("click", () => modal(myButton.id));
     }
   } catch (error) {
     console.log("Network error");
@@ -33,10 +35,22 @@ async function readAll() {
 }
 readAll();
 
-async function modal() {
+async function modal(id) {
+  try {
+    const response = await fetch(`${API_URL}/` + id, { method: "GET" });
+    const data = await response.json();
+    console.log("data: ", data);
+    let info = data;
+    document.getElementById("id").value = info.id;
+    document.getElementById("nombre").value = info.nombre;
+    document.getElementById("descripcion").value = info.descripcion;
+    document.getElementById("ingredientes").value = info.ingredientes;
+    document.getElementById("tiempo").value = info.tiempo_min;
+    document.getElementById("dificultad").value = info.dificultad;
 
-  await readAll();
-
+  } catch (error) {
+    console.log("Error cargando los datos");
+  }
   //get the modal
   var modal = document.getElementById("modal");
   //get the button that opens the modal
@@ -62,4 +76,67 @@ async function modal() {
     }
   }
 }
-modal();
+
+
+
+document.getElementById("formulario").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target)
+
+  try {
+    const response = await fetch(`${API_URL}/` + formData.get("id"), { method: "GET" });
+    const data = await response.json();
+    console.log("data: ", data);
+    let infoModal = data;
+    document.getElementById("id").value = infoModal.id;
+    document.getElementById("nombre").value = infoModal.nombre;
+    document.getElementById("descripcion").value = infoModal.descripcion;
+    document.getElementById("ingredientes").value = infoModal.ingredientes;
+    document.getElementById("tiempo").value = infoModal.tiempo_min;
+    document.getElementById("dificultad").value = infoModal.dificultad;
+
+    // Comparacion de datos
+    let cambios = {};
+    if (formData.get("nombre") !== infoModal.nombre) {
+      cambios.nombre = formData.get("nombre");
+    }
+    if (formData.get("descripcion") !== infoModal.descripcion) {
+      cambios.descripcion = formData.get("descripcion");
+    }
+    if (formData.get("ingredientes") !== infoModal.ingredientes) {
+      cambios.ingredientes = formData.get("ingredientes");
+    }
+    if (Number(formData.get("tiempo")) !== infoModal.tiempo_min) {
+      cambios.tiempo_min = Number(formData.get("tiempo"));
+    }
+    if (formData.get("dificultad") !== infoModal.dificultad) {
+      cambios.dificultad = formData.get("dificultad");
+    }
+    console.log("Hemos llegado hasta cambios: ", cambios);
+
+    //Patch en el caso de que haya cambios
+    if (Object.keys(cambios).length > 0) {
+      const r = await fetch(`${API_URL}/` + formData.get("id"), {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(cambios),
+      });
+      const datos_finales = await r.json();
+      console.log("Datos actualizados:", datos_finales);
+      document.getElementById("nombre").value = datos_finales.nombre;
+      document.getElementById("descripcion").value = datos_finales.descripcion;
+      document.getElementById("ingredientes").value = datos_finales.ingredientes;
+      document.getElementById("tiempo").value = datos_finales.tiempo_min;
+      document.getElementById("dificultad").value = datos_finales.dificultad;
+    }
+
+  } catch (error) {
+    console.log("Error cargando los datos");
+  }
+
+
+})
+
+
+
+
