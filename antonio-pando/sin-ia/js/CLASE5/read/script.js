dfe50df538a08615dad2df5f46b7d3b2400eef55
AntonioPando <antoniogpando@gmail.com>
2026-03-16 14:08:56 +0100

    update terminado

--- antonio-pando/sin-ia/js/CLASE5/read/script.js
+++ antonio-pando/sin-ia/js/CLASE5/read/script.js
@@ -1,25 +1,33 @@
-const API_URL = "https://localhost:3000/repices"
+const API_URL = "http://localhost:3000/recipes"
 
 async function loadData() {
     try{
         let response = await fetch(API_URL);
         let data = await response.json();
+
         let tamano = data.length;
 
         let text = "";
-
         data.forEach(receta => {
             text += `
-            <div class="tarjeta">
-                <img src="${receta.image_url}" alt="Imagen de ${receta.nombre}"/>
-                <div class="detalles">
-                    <p>RECETA: ${receta.nombre}</p>
-                    <p>INGREDIENTES: ${receta.ingredientes}</p>
-                    <p>TIEMPO REQUERIDO: ${receta.tiempo_min} minutos</p>
-                    <p>DESCRIPCION: ${receta.descripcion}</p>
-            </div>
+            <a href="${API_URL}/${receta.id}">
+                <div class="tarjeta">
+                    <img src="${receta.image_url}" alt="Imagen de ${receta.nombre}"/>
+                    <div class="detalles">
+                        <p>RECETA: ${receta.nombre}</p>
+                        <p>INGREDIENTES: ${receta.ingredientes}</p>
+                        <p>TIEMPO REQUERIDO: ${receta.tiempo_min} minutos</p>
+                        <p>DESCRIPCION: ${receta.descripcion}</p>
+                    </div>
+                </div>
+            <a>
             `
         })
+        document.getElementById("contenido").innerHTML = text;
 
+    } catch (error) {
+        console.log("Network error: ", error);
     }
 }
+
+loadData()
