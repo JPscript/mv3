944c67258a456000400ea984a55c8eaf59415857
GuillaumeEven <guillaume@evendev.net>
2026-03-17 18:42:39 +0100

    chore(gui); install restaurant Angular components

---
+++ guillaume/sin-ia/angular/restaurants/src/app/components/pages/home/delete-restaurant/delete-restaurant.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-restaurant',
+  imports: [],
+  templateUrl: './delete-restaurant.html',
+  styleUrl: './delete-restaurant.css',
+})
+export class DeleteRestaurant {}
---
+++ guillaume/sin-ia/angular/restaurants/src/app/components/pages/home/single-restaurant/single-restaurant.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-restaurant',
+  imports: [],
+  templateUrl: './single-restaurant.html',
+  styleUrl: './single-restaurant.css',
+})
+export class SingleRestaurant {}
