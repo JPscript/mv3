7bb806a1c5888c51c781b695fb7431d3b75a81e3
AlvaroRinki <164490545+AlvaroRinki@users.noreply.github.com>
2026-03-17 20:28:11 +0100

    feat: add footer and header components with tests
    feat: implement restaurant card component with styles and tests
    
    feat: create home page with restaurant cards display
    
    feat: add create, delete, update restaurant components with basic structure
    
    feat: implement login and registration components with styles and tests
    
    feat: add map and profile components with basic structure and styles

---
+++ alvaro/sin-ia/angular/restaurants/src/app/components/pages/home/restaurant/restaurant.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant',
+  imports: [],
+  templateUrl: './restaurant.html',
+  styleUrl: './restaurant.css',
+})
+export class Restaurant {}
