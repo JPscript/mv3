cb63d511fc7967f26ceffa331b391de85c957a88
carlosalbertoparejo <parejoc@ymail.com>
2026-03-18 20:21:00 +0100

    fin de clase

--- carlos/sin-ia/angular/fakeadvisor/src/app/components/layout/header/header.component.ts
+++ carlos/sin-ia/angular/fakeadvisor/src/app/components/layout/header/header.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { restaurantes } from '../../../data/restaurantes'; // ajusta la ruta
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, FormsModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
+  searchText = '';
+  sugerencias: any[] = [];
+
+  listaRestaurantes = restaurantes; // tu array completo
+
+  buscar() {
+    const texto = this.searchText.toLowerCase();
+
+    if (texto.length === 0) {
+      this.sugerencias = [];
+      return;
+    }
+
+    this.sugerencias = this.listaRestaurantes.filter(r =>
+      r.nombre.toLowerCase().includes(texto)
+    );
+  }
+
+  abrirEnMaps(restaurante: any) {
+    const url = `https://www.google.com/maps/search/?api=1&query=${restaurante.direccion}`;
+    window.open(url, '_blank');
+    this.sugerencias = [];
+    this.searchText = '';
+  }
 }
