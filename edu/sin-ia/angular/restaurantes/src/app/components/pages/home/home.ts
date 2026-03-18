97279b01e5a20fc9117a9d37daf96606989b7881
EDZ94 <edzlopezmorales@gmail.com>
2026-03-18 20:22:24 +0100

    Terminar header, footer y cards

--- edu/sin-ia/angular/restaurantes/src/app/components/pages/home/home.ts
+++ edu/sin-ia/angular/restaurantes/src/app/components/pages/home/home.ts
@@ -1,9 +1,49 @@
 import { Component } from '@angular/core';
+import { RestauranteCard } from './components/restaurante-card/restaurante-card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RestauranteCard],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  listadoRestaurantes = [
+    {
+      id: 1,
+      nombre: 'Restaurante A',
+      descripcion: 'Descripción del Restaurante A',
+      img: 'https://placehold.co/300x200',
+      coordenadas: { lat: 40.7128, lng: -74.0060 }
+    },
+    {
+      id: 2,
+      nombre: 'Restaurante B',
+      descripcion: 'Descripción del Restaurante B',
+      img: 'https://placehold.co/300x200',
+      coordenadas: { lat: 40.7128, lng: -74.0060 }
+    },
+    {
+      id: 3,
+      nombre: 'Restaurante C',
+      descripcion: 'Descripción del Restaurante C',
+      img: 'https://placehold.co/300x200',
+      coordenadas: { lat: 40.7128, lng: -74.0060 }
+    },
+    {
+      id: 4,
+      nombre: 'Restaurante D',
+      descripcion: 'Descripción del Restaurante D',
+      img: 'https://placehold.co/300x200',
+      coordenadas: { lat: 40.7128, lng: -74.0060 }
+    },
+    {
+      id: 5, // Asegúrate de que el ID sea 5
+      nombre: 'Restaurante E',
+      descripcion: 'Descripción del Restaurante E',
+      img: 'https://placehold.co/300x200',
+      coordenadas: { lat: 40.7128, lng: -74.0060 }
+    }
+  ];
+}
