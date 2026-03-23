6a4a2bff4355f319f205678f4a96fc7e96db92d2
Cintia Jiménez <cijimar@gmail.com>
2026-03-23 13:20:52 +0100

    feat: add header, footer, home done. crear restaurante in progress.

---
+++ cintia/sin-ia/angular/restaurantes/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/layout/header/header.component'; 
+import { FooterComponent } from './components/layout/footer/footer.component'; 
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    RouterOutlet,    
+    FooterComponent,
+  ],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class AppComponent {
+  title = 'restaurantes';
+}
+
--- cintia/sin-ia/angular/restaurantes/src/app/app.routes.ts
+++ cintia/sin-ia/angular/restaurantes/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/pages/home/home';
+import { Restaurante } from './components/pages/home/restaurante/restaurante';
+import { ActualizarRestaurante } from './components/pages/home/actualizar-restaurante/actualizar-restaurante';
+import { Login } from './components/pages/login/login';
+import { Mapa } from './components/pages/mapa/mapa';
+import { Perfil } from './components/pages/perfil/perfil';
+import { CrearRestaurante } from './components/pages/home/crear-restaurante/crear-restaurante';
+import { Registro } from './components/pages/registro/registro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/restaurantes', pathMatch: 'full'},
+    {path: 'restaurantes', title: "La cocina de la mama 🍴", component: Home},
+    {path: 'restaurantes/restaurante', title: "La cocina de la mama 🍴", component: Restaurante},
+    {path: 'restaurantes/crear-restaurante', title: "La cocina de la mama 🍴", component: CrearRestaurante},
+    {path: 'restaurantes/actualizar-restaurante', title: "La cocina de la mama 🍴", component: ActualizarRestaurante},
+    {path: 'login', title: "Login 🔐", component: Login},
+    {path: 'registro', title: "Registro 📝", component: Registro},
+    {path: 'perfil', title: "Perfil 🤓", component: Perfil},
+    {path: 'mapa', title: "Mapa 🌎", component: Mapa},
+    {path: '**', redirectTo: '/restaurantes'},
+];
