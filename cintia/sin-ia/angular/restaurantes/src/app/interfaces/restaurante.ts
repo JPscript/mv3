f0d7338ab29f93fda0d1ed73eebac30e652a6919
Cintia Jiménez <cijimar@gmail.com>
2026-03-24 20:20:44 +0100

    feat: add calificacion and recetas in services & interface

---
+++ cintia/sin-ia/angular/restaurantes/src/app/interfaces/restaurante.ts
@@ -0,0 +1,14 @@
+export interface Restaurante {
+    id: number;
+    nombre: string;
+    descripcion: string;
+    fotografia_url: string;
+    latitud: number;
+    longitud: number;
+    total_recetas?: number;
+    rating_summary?: {
+        average: number;
+        count: number;
+        distribution: Record<string, number>;
+    }
+}
