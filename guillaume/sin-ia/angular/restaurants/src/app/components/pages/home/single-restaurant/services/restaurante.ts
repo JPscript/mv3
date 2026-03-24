91eb9620202e71bf82e9a71608013fca5fad6602
GuillaumeEven <guillaume@evendev.net>
2026-03-24 17:48:03 +0100

    feat(guillaume): router until single-restaurant

--- guillaume/sin-ia/angular/restaurants/src/app/components/pages/home/single-restaurant/services/restaurante.ts
+++ guillaume/sin-ia/angular/restaurants/src/app/components/pages/home/single-restaurant/services/restaurante.ts
@@ -2,16 +2,16 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { Restaurante } from '../../../../interfaces/restaurante';
+import { Restaurante } from '../../../../../interfaces/restaurante';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Restaurantes {
+export class RestauranteService {
   private readonly http = inject(HttpClient);
   private readonly apiUrl = 'http://localhost:3000';
 
-  getAll(): Observable<Restaurante[]> {
-    return this.http.get<Restaurante[]>(`${this.apiUrl}/restaurants`);
+  getRestaurant(id: number): Observable<Restaurante> {
+    return this.http.get<Restaurante>(`${this.apiUrl}/restaurants/${id}`);
   }
 }
--- guillaume/sin-ia/angular/restaurants/src/app/components/pages/home/single-restaurant/single-restaurant.ts
+++ guillaume/sin-ia/angular/restaurants/src/app/components/pages/home/single-restaurant/single-restaurant.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, OnInit, Inject, Component, inject } from '@angular/core';
+import { RestauranteService } from './services/restaurante';
+import { Restaurante } from '../../../../interfaces/restaurante';
+
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single-restaurant',
@@ -6,4 +10,55 @@ import { Component } from '@angular/core';
   templateUrl: './single-restaurant.html',
   styleUrl: './single-restaurant.css',
 })
-export class SingleRestaurant {}
+export class SingleRestaurant implements OnInit{
+
+  private readonly restauranteService = inject(RestauranteService);
+
+  private readonly changeDetectorRef = inject(ChangeDetectorRef);
+
+  public restaurante?: Restaurante | null = null;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const idString = this.route.snapshot.paramMap.get('id');
+    const id = idString ? parseInt(idString, 10) : null;
+
+    if (id) {
+      this.getRestaurante(id);
+    }
+
+    // ou, pour réagir aux changements dynamiques :
+    // this.route.paramMap.subscribe(params => { const id = params.get('id'); });
+  }
+
+
+  isLoading = false;
+
+  errorMessage = '';
+
+  // ngOnInit(): void {
+  //   this.getRestaurante(id);
+  // }
+
+
+
+  getRestaurante(id: number): void {
+    this.isLoading = true;
+    this.restauranteService.getRestaurant(id).subscribe ({
+      next: (restaurante) => {
+        this.restaurante = restaurante;
+        console.log(this.restaurante);
+
+        this.isLoading = false;
+        this.changeDetectorRef.detectChanges();
+      },
+      error: () => {
+        this.errorMessage = "my error message";
+
+        this.isLoading = false;
+        this.changeDetectorRef.detectChanges();
+      }
+    });
+  }
+}
