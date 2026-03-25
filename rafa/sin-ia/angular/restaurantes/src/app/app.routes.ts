0e51fd5d38af363fff1e1bd762ead44b2db4c953
Rafagar123 <rafagarrido17@gmail.com>
2026-03-25 20:21:31 +0100

    feat(auth): agregar autenticación con guards, interceptors y servicios

--- rafa/sin-ia/angular/restaurantes/src/app/app.routes.ts
+++ rafa/sin-ia/angular/restaurantes/src/app/app.routes.ts
@@ -1,23 +1,25 @@
 import { Routes } from '@angular/router';
-import {Home} from './components/pages/home/home';
-import {Restaurante} from './components/pages/home/restaurante/restaurante';
+import { Home } from './components/pages/home/home';
+import { Restaurante } from './components/pages/home/restaurante/restaurante';
 import { CrearRestaurantte } from './components/pages/home/crear-restaurantte/crear-restaurantte';
 import { ActualizarRestaurante } from './components/pages/home/actualizar-restaurante/actualizar-restaurante';
 import { Login } from './components/pages/login/login';
 import { Registro } from './components/pages/registro/registro';
 import { Perfil } from './components/pages/perfil/perfil';
 import { Mapa } from './components/pages/mapa/mapa';
+import { authGuard } from './guards/auth.guard';
+import { BorrarRestaurante } from './components/pages/home/borrar-restaurante/borrar-restaurante';
 
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/restaurantes', pathMatch: 'full'},
-    {path: 'restaurantes', title: "Restaurantes", component: Home},
-    {path: 'restaurantes/restaurante', title: "Restaurantes", component: Restaurante},
-    {path: 'restaurantes/crear-restaurante', title: "Restaurantes", component: CrearRestaurantte},
-    {path: 'restaurantes/actualizar-restaurante', title: "Restaurantes", component: ActualizarRestaurante},
-    {path: 'login', title: "Login", component: Login},
-    {path: 'registro', title: "Registro", component: Registro},
-    {path: 'perfil', title: "Perfil", component: Perfil},
-    {path: 'mapa', title: "Mapa", component: Mapa },
-    {path: '**', redirectTo: '/restaurantes' }
+    { path: '', redirectTo: '/restaurantes', pathMatch: 'full' },
+    { path: 'restaurantes', title: "Restaurantes", component: Home },
+    { path: 'restaurantes/restaurante/:id', title: "Restaurantes", component: Restaurante },
+    { path: 'restaurantes/crear-restaurante', title: "Restaurantes", component: CrearRestaurantte },
+    { path: 'restaurantes/actualizar-restaurante', title: "Restaurantes", component: ActualizarRestaurante },
+    { path: 'login', title: "Login", component: Login },
+    { path: 'registro', title: "Registro", component: Registro },
+    { path: 'perfil', title: "Perfil", component: Perfil },
+    { path: 'mapa', title: "Mapa", component: Mapa },
+    { path: '**', redirectTo: '/restaurantes' }
 ];
